63cd0fb3ebb06b8db15a47bc65ca1c65cd5b26c0
Ersel <52753296+erselgemici@users.noreply.github.com>
2025-08-01 23:42:02 +0300

    Add files via upload

---
+++ DataAccessLayer/Migrations/202508011301071_mig_add_messages_isread.cs
@@ -0,0 +1,18 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig_add_messages_isread : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Messages", "IsRead", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Messages", "IsRead");
+        }
+    }
+}
---
+++ DataAccessLayer/Migrations/202508011652598_CreateTalentTable.cs
@@ -0,0 +1,29 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateTalentTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Talents",
+                c => new
+                    {
+                        TalentID = c.Int(nullable: false, identity: true),
+                        Title = c.String(maxLength: 100),
+                        Description = c.String(maxLength: 100),
+                        SkillList = c.String(maxLength: 1000),
+                        ImageUrl = c.String(maxLength: 100),
+                    })
+                .PrimaryKey(t => t.TalentID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Talents");
+        }
+    }
+}
